1b1135cfb0cb03853bc55eeafd0414327232ecf4
Robbie McLaughlin <robbie_soccer@icloud.com>
2021-10-09 20:54:11 -0500

    Added MealItem and form&input

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Meals from "./Meals";
-import classes from "./AvailableMeals.module.css"
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
   {
@@ -30,13 +31,22 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-  return <section className={classes.meals}>
-    <ul>
-      {mealsList}
-    </ul>
-  </section>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
